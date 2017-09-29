f1b8b54dcbd39f5026eb39e89f24292ab1e63d16
Laksito <arif.laksito@amikom.ac.id>
2017-09-29 10:01:58 +0700

    memahami activity lifecycle

--- AmikomApp/app/src/main/java/net/ariflaksito/amikomapp/MainActivity.java
+++ AmikomApp/app/src/main/java/net/ariflaksito/amikomapp/MainActivity.java
@@ -16,37 +16,38 @@ public class MainActivity extends AppCompatActivity {
         Log.d(msg, "The onCreate() event");
     }
 
-    /** Called when the activity is about to become visible. */
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(msg, "The onStart() event");
     }
 
-    /** Called when the activity has become visible. */
     @Override
-    protected void onResume() {
+    protected void onResume(){
         super.onResume();
         Log.d(msg, "The onResume() event");
     }
 
-    /** Called when another activity is taking focus. */
     @Override
-    protected void onPause() {
+    protected void onPause(){
         super.onPause();
         Log.d(msg, "The onPause() event");
     }
 
-    /** Called when the activity is no longer visible. */
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
         Log.d(msg, "The onStop() event");
     }
 
-    /** Called just before the activity is destroyed. */
     @Override
-    public void onDestroy() {
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(msg, "The onRestart() event");
+    }
+
+    @Override
+    protected void onDestroy(){
         super.onDestroy();
         Log.d(msg, "The onDestroy() event");
     }
