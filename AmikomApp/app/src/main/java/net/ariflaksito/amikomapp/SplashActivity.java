0232b40e930d56597e3af286c7c2c01015b823fe
ariflaksito <arif.laksito@gmail.com>
2017-10-06 10:01:33 +0700

    add splash activity, add login activity

--- AmikomApp/app/src/main/java/net/ariflaksito/amikomapp/SplashActivity.java
+++ AmikomApp/app/src/main/java/net/ariflaksito/amikomapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package net.ariflaksito.amikomapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -16,5 +17,21 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
     }
 }
